d4c157adc0efef7ddc45078518edbea8d4f61a34
Sbessonov <52399828+Sbessonov@users.noreply.github.com>
2020-01-11 01:42:48 +0300

    Comments

--- StarDust/User/views.py
+++ StarDust/User/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponseRedirect
 from .models import Article, Comment
-
+from django.urls import reverse
 
 # Create your views here.
 
@@ -16,4 +16,19 @@ def detail(request, article_id):
         a = Article.objects.get(id=article_id)
     except Exception:
         raise Http404('Статья не найдена')
-    return render(request, 'articles/detail.html', {'article': a})
+    comments_list = a.comment_set.order_by('-id')
+    return render(request, 'articles/detail.html', {'article': a, 'comments': comments_list})
+
+
+def register_user(request):
+    pass
+
+
+def leave_comment(request, article_id):
+    try:
+        a = Article.objects.get(id=article_id)
+    except Exception:
+        raise Http404('Статья не найдена')
+    a.comment_set.create(author_name=request.POST['name'], comment_text=request.POST['text'])
+
+    return HttpResponseRedirect(reverse('detail', args=(a.id,)))
