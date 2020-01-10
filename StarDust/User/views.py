d84b34b11fc0853532f50f3744ec0b2b921824ce
Sbessonov <52399828+Sbessonov@users.noreply.github.com>
2020-01-10 22:17:09 +0300

    fix_urls_and_id

--- StarDust/User/views.py
+++ StarDust/User/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404, HttpResponseRedirect
+from .models import Article, Comment
+
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, '../templates/mainpage.html', locals())
+    latest = Article.objects.order_by('-pub_date')[:2]
+    return render(request, 'articles/list_articles.html', {'latest': latest})
+
+
+def detail(request, article_id):
+    a = Article.objects.order_by('-pub_date')[:3]
+    return render(request, 'articles/list_articles.html', {'latest': a})
