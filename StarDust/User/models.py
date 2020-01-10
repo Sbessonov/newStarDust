ef831ed2d9af6e0745092b1ef708b87bb5458091
Sbessonov <52399828+Sbessonov@users.noreply.github.com>
2020-01-10 20:35:13 +0300

    Add_Article_and_Comment(Migrations)

--- StarDust/User/models.py
+++ StarDust/User/models.py
@@ -8,3 +8,25 @@ class User(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField()
     password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    article_title = models.CharField('название статьи', max_length=30)
+    author_text = models.TextField('текст статьи')
+    pub_date = models.DateTimeField('дата публикации')
+
+    def __str__(self):
+        return self.article_title
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author_name = models.CharField('имя автора', max_length=50)
+    comment_text = models.CharField('текст комментария', max_length=200)
+
+    def __str__(self):
+        return self.author_name
